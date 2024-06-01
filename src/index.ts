e09f02261fc116adf5a2a056faf853734f263876
Jomarth <117119030+Jomarth@users.noreply.github.com>
2024-06-01 01:20:55 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import mongoose from 'mongoose';
@@ -16,6 +16,10 @@ mongoose.connect(process.env.DB_CONNECTION_STRING as string)
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get('/health', async (req: Request, res: Response) => {
+    res.send({ message: 'servidor OK' })
+})
 
 app.use('/api/direcciones/', direccionRoute);
 app.use('/api/tarjetas/', tarjetaRoute);
